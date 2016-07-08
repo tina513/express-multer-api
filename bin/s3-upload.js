a2f2ede87749c78d599eba45129e3c1a09cefd81
tina513 <dlnaz.ai@hotmail.com>
2016-07-08 14:09:28 -0400

    Start the AWS upload using aws-sdk and dotenv
    Use the aws-sdk to do the upload with a test filename.
    Use dotenv to configure the S3 constructor in order to authenticate with S3

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,7 +1,17 @@
 'use strict';
 
+require('dotenv').config();
+
 const  fs = require('fs');
 const fileType = require('file-type');
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRTE_ACCESS_KEY,
+  }
+});
 
 const mimeType = (data) => {
   return Object.assign({
@@ -31,8 +41,19 @@ const awsUpload = (file) => {
     ContentType: file.mime,
     Key: `test/test.${file.ext}`
   };
-  return Promise.resolve(options);
-}
+
+  return new Promise((resolve, reject)=>{
+      s3.upload(options, (error, data)=>{
+        if (error) {
+          reject(error);
+        }
+
+        resolve(data);
+      });
+  });
+
+  // return Promise.resolve(options);
+};
 
 readFile(filename)
 .then((data) => {

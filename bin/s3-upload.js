79e2a5c7f1e82af4104c0c057a2b0cf665ad3a41
tina513 <dlnaz.ai@hotmail.com>
2016-07-11 09:37:46 -0400

    Save upload info in database
    Use upload model and mongoose

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -3,39 +3,12 @@
 require('dotenv').config();
 
 const  fs = require('fs');
-// const crypto = require('crypto');
-// const fileType = require('file-type');
+const mongoose = require('../app/middleware/mongoose');
+const Upload = require('../app/models/upload');
 const uploader = require('../lib/aws-s3-upload.js');
-// const AWS = require('aws-sdk');
-//
-// const s3 = new AWS.S3({
-//   credentials: {
-//     accessKeyId: process.env.AWS_ACCESS_KEY,
-//     secretAccessKey: process.env.AWS_SECRTE_ACCESS_KEY,
-//   }
-// });
-
-// const prepareFile = (data) => {
-//   return Object.assign({
-//     data,
-//     ext: 'bin',
-//     mime: 'application/octet-stream',
-//   },fileType(data));
-// };
-
-// const randomHexString = (length) => {
-//   return new Promise((resolve, reject)=>{
-//     crypto.randomBytes(length, (err, buf)=>{
-//       if (err) {
-//         reject(err);
-//       }
-//
-//       resolve(buf.toString('hex'));
-//     });
-//   });
-// };
 
 let filename = process.argv[2] || '';
+let comment = process.argv[3] || 'No comment';
 
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
@@ -48,34 +21,16 @@ const readFile = (filename) => {
   });
 };
 
-// const awsUpload = (file) => {
-//  return  randomHexString(16)
-//           .then((filename)=>{
-//             let dir = new Date().toISOString().split('T')[0];
-//              return {
-//               ACL: "public-read",
-//               Body: file.data,
-//               Bucket: 'wdibucket',
-//               ContentType: file.mime,
-//               Key: `${dir}/${filename}.${file.ext}`
-//             };
-//           })
-//           .then((options)=>{
-//             return new Promise((resolve, reject)=>{
-//                 s3.upload(options, (error, data)=>{
-//                   if (error) {
-//                     reject(error);
-//                   }
-//
-//                   resolve(data);
-//                 });
-//             });
-//           });
-//   // return Promise.resolve(options);
-// };
-
 readFile(filename)
 .then(uploader.prepareFile)
 .then(uploader.awsUpload)
+.then((response) => {
+  let upload = {
+    location: response.Location,
+    comment: comment, //not defined yet
+  };
+  return Upload.create(upload);
+})
 .then(console.log)
-.catch(console.error);
+.catch(console.error)
+.then(()=>mongoose.connection.close());
